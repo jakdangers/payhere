6d8119b1212a5c6780148087f841a03d1b582f11
jakdangers <103917719+jakdangers@users.noreply.github.com>
2024-02-20 22:31:41 +0900

    [#2] table, my.cnf 추가 (#3)

--- source/init.sql
+++ source/init.sql
@@ -1,4 +1,39 @@
-CREATE TABLE init_pings (
-    id       INT AUTO_INCREMENT PRIMARY KEY
+CREATE TABLE users
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    userid      VARCHAR(255) UNIQUE NOT NULL,
+    password    VARCHAR(255)        NOT NULL,
+    create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    delete_date TIMESTAMP           NULL
 );
 
+CREATE TABLE categories
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL,
+    parent_id   INT,
+    create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    delete_date TIMESTAMP    NULL,
+    FOREIGN KEY (parent_id) REFERENCES categories (id)
+);
+
+CREATE TABLE products
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    category_id INT,
+    user_id     INT,
+    name        VARCHAR(255),
+    initial     VARCHAR(255),
+    price       DECIMAL(10, 2),
+    description TEXT,
+    barcode     VARCHAR(50),
+    expiry_date DATE,
+    size        VARCHAR(50),
+    create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    delete_date TIMESTAMP NULL,
+    FOREIGN KEY (category_id) REFERENCES categories (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
